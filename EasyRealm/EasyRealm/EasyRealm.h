9d6b772d87c168e181cf57fbdeb87c7e81df4485
Allan Vialatte <allan.vialatte@icloud.com>
2017-03-26 16:23:09 +0200

    prepare carthage proj

---
+++ EasyRealm/EasyRealm/EasyRealm.h
@@ -0,0 +1,19 @@
+//
+//  EasyRealm.h
+//  EasyRealm
+//
+//  Created by Allan Vialatte on 26/03/2017.
+//  Copyright © 2017 Allan Vialatte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyRealm.
+FOUNDATION_EXPORT double EasyRealmVersionNumber;
+
+//! Project version string for EasyRealm.
+FOUNDATION_EXPORT const unsigned char EasyRealmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyRealm/PublicHeader.h>
+
+
